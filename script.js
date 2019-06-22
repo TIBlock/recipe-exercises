09dbf42b31fe1751efb5c7ace5967392d38bfceb
Taylor Blocker <taylorblocker@gmail.com>
2019-06-22 10:29:36 -0500

    Completed Marinara Recipe Exercise

---
+++ script.js
@@ -0,0 +1,12 @@
+function marinaraSauce(cookName){
+    var two = 2
+    var oneFourth = "1/4"
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!")
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log("- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + 2 +  " minutes.");
+    console.log("- Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.");
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+    
+}
+marinaraSauce("Taylor");
